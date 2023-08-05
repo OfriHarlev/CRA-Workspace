158a83be72da6396aa30c9ada16b09dab5e69a62
Ofri Harlev <ofri.harlev@gmail.com>
2023-08-04 18:56:54 -0700

    Repro the problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import {MantineProvider, ScrollArea} from "@mantine/core";
+
 function App() {
+  // @ts-ignore
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <MantineProvider withNormalizeCSS>
+            <ScrollArea h={200} offsetScrollbars  type="always">
+              Testadsfjasfjaslkf jflk dsjflkdsjf kljflksda flkasdfj dsalkf
+              jadsfjsdafasdfds fsdf a f sdafdsafsda fdsaf af a f fdsf dsf
+              dsf dsf dsf sda fads fdsaf asdf adsf sda fds f sad f sda f s
+              fas
+              fdsfsdfsdfsadfdsafsdfdsfsdfasdfsdffsdfdsfsdfdsafdsfasfdsfsadfdsfsadfasd
+              fds fds fds fasdf sdf ds fa fdsf sd f asf sd f ds ffasdf sfdsf
+              ds faad fdsa fsda fsd fdsa fasdfsad fds fasd fasdfa dssad fdss
+              dfsd s
+              jadsfjsdafasdfds fsdf a f sdafdsafsda fdsaf af a f fdsf dsf
+              dsf dsf dsf sda fads fdsaf asdf adsf sda fds f sad f sda f s
+              fas
+              fdsfsdfsdfsadfdsafsdfdsfsdfasdfsdffsdfdsfsdfdsafdsfasfdsfsadfdsfsadfasd
+              fds fds fds fasdf sdf ds fa fdsf sd f asf sd f ds ffasdf sfdsf
+              ds faad fdsa fsda fsd fdsa fasdfsad fds fasd fasdfa dssad fdss
+              dfsd s
+              jadsfjsdafasdfds fsdf a f sdafdsafsda fdsaf af a f fdsf dsf
+              dsf dsf dsf sda fads fdsaf asdf adsf sda fds f sad f sda f s
+              fas
+              fdsfsdfsdfsadfdsafsdfdsfsdfasdfsdffsdfdsfsdfdsafdsfasfdsfsadfdsfsadfasd
+              fds fds fds fasdf sdf ds fa fdsf sd f asf sd f ds ffasdf sfdsf
+              ds faad fdsa fsda fsd fdsa fasdfsad fds fasd fasdfa dssad fdss
+              dfsd s          jadsfjsdafasdfds fsdf a f sdafdsafsda fdsaf af a f fdsf dsf
+              dsf dsf dsf sda fads fdsaf asdf adsf sda fds f sad f sda f s
+              fas
+              fdsfsdfsdfsadfdsafsdfdsfsdfasdfsdffsdfdsfsdfdsafdsfasfdsfsadfdsfsadfasd
+              fds fds fds fasdf sdf ds fa fdsf sd f asf sd f ds ffasdf sfdsf
+              ds faad fdsa fsda fsd fdsa fasdfsad fds fasd fasdfa dssad fdss
+              dfsd s
+
+            </ScrollArea>
+          </MantineProvider>
+        </header>
     </div>
   );
 }
